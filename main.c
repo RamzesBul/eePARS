f02442dd371c19c0818c3f03c1878d899ee8e71f
Bulat_Ramazanov <ramzesbulman@gmail.com>
2023-08-29 23:07:31 +0500

    #1 Fix: rename server callback function

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <vkclient.h>
 
-static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+static void server_callback(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
   if (ev == MG_EV_HTTP_MSG) {
     struct mg_http_message *hm = (struct mg_http_message *) ev_data;
     if (mg_http_match_uri(hm, "/api/hello")) {              // On /api/hello requests,
@@ -19,7 +19,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
 void *server_thread(void *) {
   struct mg_mgr mgr;                                
   mg_mgr_init(&mgr);                                      // Init manager
-  mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);  // Setup listener
+  mg_http_listen(&mgr, "http://0.0.0.0:8000", server_callback, NULL);  // Setup listener
   for (;;) mg_mgr_poll(&mgr, 1000);                       // Infinite event loop
 }
 
