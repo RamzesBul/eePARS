7db446a19854b9498ef9547e001dcb9368057ce1
Bulat_Ramazanov <ramzesbulman@gmail.com>
2024-02-01 23:16:59 +0500

    eepars: add build script for mbedtls
    Signed-off-by: Bulat_Ramazanov <ramzesbulman@gmail.com>

--- build.cmd
+++ build.cmd
@@ -1,1 +1,39 @@
+@echo off
+
+set MBEDTLS_LIB_FOLDER="back\mbedtls\"
+set EXTERNAL_FOLDER="include\external_projects"
+
+cd /d "%~dp0"
+for %%i in (%MBEDTLS_LIB_FOLDER%"libmbedcrypto.a", %MBEDTLS_LIB_FOLDER%"libmbedtls.a", %MBEDTLS_LIB_FOLDER%"libmbedx509.a"
+) do if not exist "%%~i" echo\File "%%~i" does Not exist && goto %:^(
+
+:: Run more commands below, because all your files exist...
+echo The files exist, you don't need to do anything.
+
+goto :begin
+
+%:^(
+:: Run more commands below, because one some of your files don't exist
+echo The files don't exist, I start cloning Mbed TLS and build it up.
+
+if not exist "back\mbedtls\" (
+    cd back\
+    git clone -b v2.28.6 https://github.com/Mbed-TLS/mbedtls.git
+    cd mbedtls\
+    cmake -S . -B build\ -G "MinGW Makefiles"
+    cmake --build build\ --target mbedtls --config Release
+    copy build\library\*.a ..\lib\mbedtls\
+    copy build\include\mbedtls\*.h ..\%EXTERNAL_FOLDER%\mbedtls\
+    copy build\include\psa\*.h ..\%EXTERNAL_FOLDER%\psa\
+
+    cd ../
+    rd /s /q mbedtls
+    cd ../
+) else (
+    echo mbedtls already exist.
+)
+
+:begin
+
+:: Build docker image
 docker image build . -t eepars
