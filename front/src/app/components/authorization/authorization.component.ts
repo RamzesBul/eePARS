a1fa453dde22c78772f0d4260ce156ac086aa0a3
cMeWHou <cmewhou@yandex.ru>
2024-03-03 02:12:14 +0500

    fix(configuration): fix server address on remote.
    fix(configuration): change remote server address.
    
    fix(config): fix server address.
    
    fix(configuration): change remote server address.

--- front/src/app/components/authorization/authorization.component.ts
+++ front/src/app/components/authorization/authorization.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { IUserProfile } from 'src/app/services/core.service';
+import { CoreService, IUserProfile } from 'src/app/services/core.service';
 
 @Component({
   selector: 'app-authorization',
@@ -10,17 +10,18 @@ import { IUserProfile } from 'src/app/services/core.service';
   styleUrls: ['./authorization.component.scss']
 })
 export class AuthorizationComponent {
-  public userInfo$: Observable<{ response: IUserProfile }> = this.http.get<{ response: IUserProfile }>('http://localhost:8080/authorization', {
-    params: {
-      'access_token': (() => {
-        const url: string = this.router.url;
-        const accessTokenStart: number = url.indexOf('access_token') + 'access_token'.length + 1;
-        const accessTokenEnd: number = url.indexOf('expires_in') - 1;
+  public userInfo$: Observable<{ response: IUserProfile }> =
+    this.http.get<{ response: IUserProfile }>(`${this.coreService.applicationInfo['serverUrl']}/authorization`, {
+      params: {
+        'access_token': (() => {
+          const url: string = this.router.url;
+          const accessTokenStart: number = url.indexOf('access_token') + 'access_token'.length + 1;
+          const accessTokenEnd: number = url.indexOf('expires_in') - 1;
 
-        return url.substring(accessTokenStart, accessTokenEnd);
-      })()
-    }
-  });
+          return url.substring(accessTokenStart, accessTokenEnd);
+        })()
+      }
+    });
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private coreService: CoreService, private router: Router) { }
 }
